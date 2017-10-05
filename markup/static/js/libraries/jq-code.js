215ec8b289f3caa141e9e461c256f619459ffa78
Pavel <shirokuiuna@mail.ru>
2017-10-05 22:43:11 +0300

    upload

--- markup/static/js/libraries/jq-code.js
+++ markup/static/js/libraries/jq-code.js
@@ -25,25 +25,23 @@ $(function () {
   
   var nav = $('.page-header__blockDescription-rowTop');
   
+  $(window).scroll(function () {
+    var i = 0;
+    var footerPos = $('footer').offset().top;
+    i = $(this).scrollTop() + 1;
+    
+    if (i > footerPos) {
+      nav.addClass('footer-nav');
+    } else {
+      nav.removeClass('footer-nav');
+    }
+  });
+  
   $(document).scroll(function () {
     if ($(document).scrollTop() > $('header').height() -1) {
       nav.addClass('page-header__blockDescription-rowTop--fixed');
     } else {
       nav.removeClass('page-header__blockDescription-rowTop--fixed');
-    }
-  });
-  
-  
-  var target = $('#fromFixedNav');
-  var targetPos = target.offset().top;
-  var winHeight = $(window).height();
-  var scrollToElem = targetPos - winHeight;
-  $(window).scroll(function(){
-    var winScrollTop = $(this).scrollTop();
-    if(winScrollTop > scrollToElem){
-      nav.addClass('footer-nav');
-    } else {
-      nav.removeClass('footer-nav');
     }
   });
   
@@ -55,37 +53,4 @@ $(function () {
   portfolioCart.on('mouseleave', function () {
     $(this).removeClass('portfolio__cart--hover');
   });
-  
-  /*$(window).scroll(function(){
-   var servicesLink = $('.page-header__blockDescription-rowTop');
-     if ( $(window).scrollTop() > 600 ){
-        servicesLink.addClass('page-header__blockDescription-rowTop--fixed');
-     } else {
-        servicesLink.removeClass('page-header__blockDescription-rowTop--fixed');
-     }
-  });
-  $(window).scroll(function(){
-   var servicesSocialLinkFb = $('.services__socialLink--fb');
-     if ( $(window).scrollTop() > 50 ){
-        servicesSocialLinkFb.addClass('services__socialLink--fbScroll');
-     } else {
-        servicesSocialLinkFb.removeClass('services__socialLink--fbScroll');
-     }
-  });
-  $(window).scroll(function(){
-   var servicesSocialLinkInst = $('.services__socialLink--inst');
-     if ( $(window).scrollTop() > 110 ){
-        servicesSocialLinkInst.addClass('services__socialLink--instScroll');
-     } else {
-        servicesSocialLinkInst.removeClass('services__socialLink--instScroll');
-     }
-  });
-  $(window).scroll(function(){
-   var servicesSocialLinkIn = $('.services__socialLink--in');
-     if ( $(window).scrollTop() > 170 ){
-        servicesSocialLinkIn.addClass('services__socialLink--inScroll');
-     } else {
-        servicesSocialLinkIn.removeClass('services__socialLink--inScroll');
-     }
-  });*/
 });
